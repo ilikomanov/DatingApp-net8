857d0991b7e9cf4dd33871134b562f08620bd760
iliyalikomanov <iliyalikomanov@gmail.com>
2024-08-23 12:27:06 +0300

    Initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //[controller] will be replaced with the name of the class - /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")] //api/users/1   {id:int} - the 'int' is type safety, so that the 'id' is integer
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
