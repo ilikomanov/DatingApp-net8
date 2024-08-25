4d02b89a2bed6388c84c0cd0dfd09bfb4e56a07a
iliyalikomanov <iliyalikomanov@gmail.com>
2024-08-25 10:04:22 +0300

    Created "ITokenService" interface with "CreateToken" method. Created "TokenService" method which implements "ITokenService" interface. "Program.cs" - impelemnted "builder.Services.AddScoped<ITokenService, TokenService>();".

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
