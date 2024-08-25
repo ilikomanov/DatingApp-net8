1d99a4aeec95728230c34b4b532b2fc087890fa4
iliyalikomanov <iliyalikomanov@gmail.com>
2024-08-25 14:32:43 +0300

    Created "UserDto" class. "AccountController" class - code redone to use "UserDto" and create a token. "TokenService" class - typo fixed at line 14.

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -11,7 +11,7 @@ public class TokenService(IConfiguration config) : ITokenService
 {
     public string CreateToken(AppUser user)
     {
-        var tokenKey = config["TonekKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
+        var tokenKey = config["TokenKey"] ?? throw new Exception("Cannot access tokenKey from appsettings");
         if (tokenKey.Length < 64) throw new Exception("Your tokenKey needs to be longer");
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
 
