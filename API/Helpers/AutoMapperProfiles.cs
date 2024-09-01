7fd6a1321895fda1903040d660638ba66b3aa934
iliyalikomanov <iliyalikomanov@gmail.com>
2024-09-01 18:28:28 +0300

    Class "Photo.cs" - fixed type into "IsMain" property. DataBase updated.

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -7,8 +7,10 @@ namespace API.Helpers;
 public class AutoMapperProfiles : Profile
 {
     public AutoMapperProfiles()
-    {//CreateMap<from where to go, to where to go>();
-        CreateMap<AppUser, MemberDto>(); //from AppUser to MemberDto
+    {// CreateMap<from where to go, to where to go>();
+        CreateMap<AppUser, MemberDto>() //from AppUser to MemberDto
+            .ForMember(d => d.PhotoUrl, o => 
+                o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url)); //! - null forgiving operator
         CreateMap<Photo, PhotoDto>();
     }
 }
