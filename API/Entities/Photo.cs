3fd1e987088cdd20da32b44cd4364c820eced890
iliyalikomanov <iliyalikomanov@gmail.com>
2024-09-01 15:47:11 +0300

    "Photo" class - added atribute for table name and "AppUserId", "AppUser" properties - for relationship between "User" and "Photo".

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
     public required string Url { get; set; }
     public bool IsMian { get; set; }
     public string? PublicId { get; set; }
+
+    //Navigation properties
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; } = null!; //null forgiving operator
+
 }
