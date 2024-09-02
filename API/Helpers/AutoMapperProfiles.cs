4647efd36289b3c9977cb724cb5a30e813bce3f8
iliyalikomanov <iliyalikomanov@gmail.com>
2024-09-02 21:41:41 +0300

    "IUserRepository" interface - added "GetMembersAsync" and "GetMemberAsync" methods. "AppUser" class - "GetAge" method was commented. "AutoMapperProfiles" class - "CalculateAge" added as queriable.

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using AutoMapper;
 
 namespace API.Helpers;
@@ -9,7 +10,8 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {// CreateMap<from where to go, to where to go>();
         CreateMap<AppUser, MemberDto>() //from AppUser to MemberDto
-            .ForMember(d => d.PhotoUrl, o => 
+            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+            .ForMember(d => d.PhotoUrl, o =>
                 o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url)); //! - null forgiving operator
         CreateMap<Photo, PhotoDto>();
     }
