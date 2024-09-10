f11c01caa34b273188633948e12fe53e4b7df44c
iliyalikomanov <iliyalikomanov@gmail.com>
2024-09-10 22:26:23 +0300

    Validation feedback added.

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -9,12 +9,12 @@ import {
 } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -31,7 +31,7 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl('Hello', Validators.required),
+      username: new FormControl('', Validators.required),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
