dce1a51a746dfb0ba7542f908e0ea57d020d3e01
iliyalikomanov <iliyalikomanov@gmail.com>
2024-09-14 20:05:12 +0300

    Action filter added. "LogUserActivity" class created. "BaseApiController", "ApplicationServiceExtensions" classes - code updated.

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,7 +1,9 @@
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")] //[controller] will be replaced with the name of the class - /api/users
 public class BaseApiController : ControllerBase
