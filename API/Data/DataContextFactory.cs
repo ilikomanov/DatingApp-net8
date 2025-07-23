f33cca745991e96a0f2b2c8c7b8cfe277df5e129
iliyalikomanov <iliyalikomanov@gmail.com>
2025-07-23 21:42:20 +0300

    Try Postgerss SQL

--- API/Data/DataContextFactory.cs
+++ API/Data/DataContextFactory.cs
@@ -10,6 +10,7 @@ namespace API.Data
             var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
 
             // Use the same database provider as your app, e.g. SQLite here
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=datingapp_db_s8cl_user;Password=xUpnGUGfrkK8SWTG6tpC4n9SQW5iwiIo;Database=datingapp");
 
 
             return new DataContext(optionsBuilder.Options);
