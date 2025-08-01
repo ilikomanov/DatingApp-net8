89a1073ffc48820972a7f267939986958f4a6820
ilikomanov <thewickermilko@gmail.com>
2025-08-01 13:10:14 +0300

    Created "DatingApp.Tests". Created 'UsersControllerTests' and added test for the first method.

---
+++ DatingApp.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using API.Controllers;
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using API.Interfaces;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace DatingApp.Tests.Controllers
+{
+    public class UsersControllerTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly Mock<IPhotoService> _mockPhotoService;
+        private readonly UsersController _controller;
+
+        public UsersControllerTests()
+        {
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _mockMapper = new Mock<IMapper>();
+            _mockPhotoService = new Mock<IPhotoService>();
+
+            _controller = new UsersController(_mockUnitOfWork.Object, _mockMapper.Object, _mockPhotoService.Object);
+
+            // Simulate a logged-in user for User.GetUsername()
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "testuser")
+            }, "mock"));
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = user }
+            };
+        }
+
+        [Fact]
+        public async Task GetUsers_ReturnsOkResult_WithPagedMembers()
+        {
+            // Arrange
+            var userParams = new UserParams();
+
+            var members = new List<MemberDto>
+            {
+                new MemberDto { Username = "user1" }
+            };
+
+            var pagedList = new PagedList<MemberDto>(
+                items: members,
+                count: 1,
+                pageNumber: 1,
+                pageSize: 10
+            );
+
+            _mockUnitOfWork.Setup(u => u.UserRepository.GetMembersAsync(It.IsAny<UserParams>()))
+                .ReturnsAsync(pagedList);
+
+            // Act
+            var result = await _controller.GetUsers(userParams);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsAssignableFrom<IEnumerable<MemberDto>>(okResult.Value);
+            returnValue.Should().HaveCount(1);
+        }
+    }
+}
