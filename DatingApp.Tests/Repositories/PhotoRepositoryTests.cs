1e2b9493adfc3adbd972129f817c94a879fc71ce
ilikomanov <thewickermilko@gmail.com>
2025-09-18 13:10:44 +0300

    Created 'PhotoRepositoryTests.cs'. Created 'public async Task GetPhotoById_ReturnsPhoto_WhenExists()'.

---
+++ DatingApp.Tests/Repositories/PhotoRepositoryTests.cs
@@ -0,0 +1,63 @@
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace DatingApp.Tests.Repositories
+{
+    public class PhotoRepositoryTests
+    {
+        private readonly DataContext _context;
+        private readonly PhotoRepository _repository;
+
+        public PhotoRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new DataContext(options);
+            _repository = new PhotoRepository(_context);
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            var user = new AppUser
+            {
+                UserName = "alice",
+                KnownAs = "Alice",
+                Gender = "female",
+                City = "Wonderland",
+                Country = "Fantasy"
+            };
+
+            _context.Users.Add(user);
+
+            _context.Photos.AddRange(
+                new Photo { Url = "http://photo1.com", AppUser = user, IsApproved = false, AppUserId = 1 },
+                new Photo { Url = "http://photo2.com", AppUser = user, IsApproved = true, AppUserId = 2 }
+            );
+
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task GetPhotoById_ReturnsPhoto_WhenExists()
+        {
+            // Arrange
+            var expectedPhoto = _context.Photos.First(); // because we know we seeded it
+
+            // Act
+            var photo = await _repository.GetPhotoById(expectedPhoto.Id);
+
+            // Assert
+            photo.Should().NotBeNull();
+            photo!.Url.Should().Be(expectedPhoto.Url);
+        }
+    }
+}
